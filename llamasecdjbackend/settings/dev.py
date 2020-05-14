30086f32b9fdf6246a00299a697d837823973294
iamllama <sahiil@protonmail.com>
2020-05-14 18:28:48 +0530

    Switched to Django for backend and frontend is going to be plain HTML, CSS, JS.

---
+++ llamasecdjbackend/settings/dev.py
@@ -0,0 +1,19 @@
+from .base import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'llamasec',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres'
+    }
+}
+
+CORS_ORIGIN_ALLOW_ALL = True
