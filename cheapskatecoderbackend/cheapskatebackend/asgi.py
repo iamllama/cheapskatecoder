a6793c1cf87dfb92afc09b5132a8667b078c26f1
iamllama <sahiil@protonmail.com>
2020-05-15 16:23:19 +0530

    renamed the project.

--- cheapskatecoderbackend/cheapskatebackend/asgi.py
+++ cheapskatecoderbackend/cheapskatebackend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for llamasecdjbackend project.
+ASGI config for cheapskatebackend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'llamasecdjbackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cheapskatebackend.settings')
 
 application = get_asgi_application()
